04a65d193a8083cf8c8c60978dcec67f6b4ac356
kmdv <jacob.wojnicky@gmail.com>
2014-01-23 18:07:14 +0100

    Init.

---
+++ Database/Companies.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Companies]
+(
+	Id int PRIMARY KEY IDENTITY,
+	Name varchar(255),
+	ResidenceAddress varchar(255)
+)
---
+++ Database/Trainers.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Trainers]
+(
+	Id int PRIMARY KEY IDENTITY,
+	FirstName varchar(255),
+	LastName varchar(255),
+	EmailAddress varchar(255)
+)
